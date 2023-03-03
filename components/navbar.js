4220884dfa6c907669273d6dcdf9b4213e04033c
Cristhian Zavala <winderzv@gmail.com>
2023-03-02 21:41:13 -0500

    updates

--- components/navbar.js
+++ components/navbar.js
@@ -107,6 +107,7 @@ export default function Navbar() {
         {show && (
           <div className="" id="mobile-menu">
             <div className="px-2 pt-2 pb-3 space-y-1">
+              {/*
               <Link
                 href="/"
                 className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
@@ -114,27 +115,7 @@ export default function Navbar() {
               >
                 Dashboard
               </Link>
-  
-              <Link
-                href="/"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Team
-              </Link>
-  
-              <Link
-                href="/"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Projects
-              </Link>
-  
-              <Link
-                href="/"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Calendar
-              </Link>
+              */}
             </div>
           </div>
         )}
